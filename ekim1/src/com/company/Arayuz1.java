900adf769d28792665c1b0bf3e497fc2958e60dc
Cihan Fırat <90216569+cihanfirat@users.noreply.github.com>
2024-10-01 18:55:09 +0300

    Delete ekim1 directory

--- ekim1/src/com/company/Arayuz1.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-//interface? ne için , ne zaman kullanılır?
-
-public interface Arayuz1 {
-    public String isim="Cihan"; //Variable 'isim' might not have been initialized, must use default value.
-    public void fonksiyon1(); //interface abstract methods cannot have body!
-    public String fonksiyon2();
-}
--- ekim1/src/com/company/Arayuz2.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface Arayuz2 {
-    public void fonksiyon3();
-    public int fonksiyon4();
-}
